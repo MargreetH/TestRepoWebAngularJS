806db32fc5e1d19553b835c7ec47a2406ff16702
Margreet <0margreet@gmail.com>
2016-11-17 16:20:17 +0100

    Klooien

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Hello Angular555!</h1>'
+      template: 'test.html'
     })
     .Class({
       constructor: function() {}
